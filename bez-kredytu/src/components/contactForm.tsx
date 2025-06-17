4e4daf1ca5602bbe36355dc28d32bea0c4591d3b
Hubert Brończyk <81415554+Fakelatency@users.noreply.github.com>
2025-06-17 17:15:40 +0200

    M1CR0

--- bez-kredytu/src/components/contactForm.tsx
+++ bez-kredytu/src/components/contactForm.tsx
@@ -118,12 +118,12 @@ const ContactForm: React.FC = () => {
                 <button
                     type="submit"
                     disabled={isSubmitting}
-                    className="bg-indigo-700 hover:bg-indigo-800 text-white font-bold py-3 px-8 rounded-lg transition mt-2 shadow disabled:bg-indigo-400"
+                    className="bg-indigo-900 hover:bg-indigo-800 text-white font-bold py-3 px-8 rounded-lg transition mt-2 shadow disabled:bg-indigo-400"
                 >
                     {isSubmitting ? 'Wysyłanie...' : 'Wyślij'}
                 </button>
                 {submitted && (
-                    <div className="text-indigo-900 font-semibold mt-2">
+                    <div className="text-indigo-900 text-center font-semibold mt-2">
                         Dziękujemy za kontakt! Odpowiemy wkrótce.
                     </div>
                 )}
